d5cf19765d66b669f2f8918afcdf63eb0922e8c6
Mark Wainwright <wainwrightml@gmail.com>
2023-03-04 21:59:50 +0000

    made extern functions final

--- src/extern_functions.rs
+++ src/extern_functions.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen()]
 extern "C" {
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "ActionSheet"], js_name = "showActions")]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "ActionSheet"], js_name = "showActions")]
     pub(crate) async fn action_sheet_show_actions(options: JsValue) -> Result<JsValue, JsValue>;
 }
 
@@ -10,46 +10,46 @@ extern "C" {
 extern "C" {
 
     /// Prompt the user to pick a photo from an album, or take a new photo with the camera.
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "getPhoto")]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "getPhoto")]
     pub(crate) async fn camera_get_photo(options: JsValue) -> Result<JsValue, JsValue>;
 
     /// Allows the user to pick multiple pictures from the photo gallery. On iOS 13 and older it only allows to pick one picture.
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "pickImages")]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "pickImages")]
     pub(crate) async fn camera_pick_images(options: JsValue) -> Result<JsValue, JsValue>;
 
     /// iOS 14+ Only: Allows the user to update their limited photo library selection. On iOS 15+ returns all the limited photos after the picker dismissal. On iOS 14 or if the user gave full access to the photos it returns an empty array.
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "Camera"],js_name = "pickLimitedLibraryPhotos")]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "Camera"],js_name = "pickLimitedLibraryPhotos")]
     pub(crate) async fn camera_pick_limited_library_photos() -> Result<JsValue, JsValue>;
 
     /// iOS 14+ Only: Return an array of photos selected from the limited photo library.
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "getLimitedLibraryPhotos")]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "getLimitedLibraryPhotos")]
     pub(crate) async fn camera_get_limited_library_photos() -> Result<JsValue, JsValue>;
 
     /// Check camera and photo album permissions
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "checkPermissions")]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "checkPermissions")]
     pub(crate) async fn camera_check_permissions() -> Result<JsValue, JsValue>;
 
     /// Request camera and photo album permissions. Not implemented on Web
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "requestPermissions")]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Camera"], js_name = "requestPermissions")]
     pub(crate) async fn camera_request_permissions(options: JsValue) -> Result<JsValue, JsValue>;
 
 }
 
 #[wasm_bindgen()]
 extern "C" {
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getId" )]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getId" )]
     pub(crate) async fn device_get_id() -> Result<JsValue, JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getInfo" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getInfo" )]
     pub(crate) async fn device_get_info() -> Result<JsValue, JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getBatteryInfo" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getBatteryInfo" )]
     pub(crate) async fn device_get_battery_info() -> Result<JsValue, JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getLanguageCode" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getLanguageCode" )]
     pub(crate) async fn device_get_language_code() -> Result<JsValue, JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getLanguageTag" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Device"], js_name="getLanguageTag" )]
     pub(crate) async fn device_get_language_tag() -> Result<JsValue, JsValue>;
 
 }
@@ -58,26 +58,26 @@ extern "C" {
 extern "C" {
 
     /// Vibrate the device
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "Haptics"])]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "Haptics"])]
     pub(crate) async fn haptics_vibrate(options: JsValue) -> Result<(), JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Haptics"])]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Haptics"])]
     pub(crate) async fn haptics_impact(options: JsValue) -> Result<(), JsValue>;
 
     /// Trigger a haptics "notification" feedback
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Haptics"])]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Haptics"])]
     pub(crate) async fn haptics_notification(options: JsValue) -> Result<(), JsValue>;
 
     /// Trigger a selection started haptic hint
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Haptics"], js_name="selectionStart")]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Haptics"], js_name="selectionStart")]
     pub(crate) async fn haptics_selectionStart() -> Result<(), JsValue>;
 
     /// Trigger a selection changed haptic hint. If a selection was started already, this will cause the device to provide haptic feedback
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Haptics"], js_name="selectionChanged")]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Haptics"], js_name="selectionChanged")]
     pub(crate) async fn haptics_selectionChanged() -> Result<(), JsValue>;
 
     /// If selectionStart() was called, selectionEnd() ends the selection. For example, call this when a user has lifted their finger from a control
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Haptics"], js_name="selectionEnd")]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Haptics"], js_name="selectionEnd")]
     pub(crate) async fn haptics_selectionEnd() -> Result<(), JsValue>;
 }
 
@@ -85,10 +85,10 @@ extern "C" {
 extern "C" {
 
     /// Schedule one or more local notifications.
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "LocalNotifications"], js_name="schedule" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "LocalNotifications"], js_name="schedule" )]
     pub(crate) async fn local_notifications_schedule(options: JsValue) -> Result<JsValue, JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "LocalNotifications"], js_name="registerActionTypes" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "LocalNotifications"], js_name="registerActionTypes" )]
     pub(crate) async fn local_notifications_register_action_types(
         options: JsValue,
     ) -> Result<(), JsValue>;
@@ -103,7 +103,7 @@ extern "C" {
 #[wasm_bindgen()]
 extern "C" {
     /// Query the current status of the network connection.
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "Network"], js_name="getStatus" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "Network"], js_name="getStatus" )]
     pub(crate) async fn network_get_status() -> Result<JsValue, JsValue>;
 
     #[wasm_bindgen(js_namespace = ["Capacitor", "Plugins", "Network"], js_name="addListener" )]
@@ -115,36 +115,36 @@ extern "C" {
 
 #[wasm_bindgen()]
 extern "C" {
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "Share"], js_name="canShare" )]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "Share"], js_name="canShare" )]
     pub(crate) async fn share_can_share() -> Result<JsValue, JsValue>;
 
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "Share"], js_name="share" )]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "Share"], js_name="share" )]
     pub(crate) async fn share_share(options: JsValue) -> Result<JsValue, JsValue>;
 }
 
 #[wasm_bindgen()]
 extern "C" {
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="show" )]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="show" )]
     pub(crate) async fn status_bar_show() -> Result<(), JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="hide" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="hide" )]
     pub(crate) async fn status_bar_hide() -> Result<(), JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="setStyle" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="setStyle" )]
     pub(crate) async fn status_bar_set_style(options: JsValue) -> Result<(), JsValue>;
 
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="setBackgroundColor" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="setBackgroundColor" )]
     pub(crate) async fn status_bar_set_background_color(options: JsValue) -> Result<(), JsValue>;
 
     /// Set whether or not the status bar should overlay the webview to allow usage of the space underneath it.
     /// This method is only supported on Android.
-    #[wasm_bindgen(catch,js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="setOverlaysWebView" )]
+    #[wasm_bindgen(catch, final,js_namespace = ["Capacitor", "Plugins", "StatusBar"], js_name="setOverlaysWebView" )]
     pub(crate) async fn status_bar_set_overlays_web_view(options: JsValue) -> Result<(), JsValue>;
 }
 
 
 #[wasm_bindgen()]
 extern "C" {
-    #[wasm_bindgen(catch, js_namespace = ["Capacitor", "Plugins", "Toast"], js_name="show" )]
+    #[wasm_bindgen(catch, final, js_namespace = ["Capacitor", "Plugins", "Toast"], js_name="show" )]
     pub(crate) async fn toast_show(options: JsValue) -> Result<(), JsValue>;
 }
